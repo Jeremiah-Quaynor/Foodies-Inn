9e6deb489ea7b550c87195ead28f767b1c8c72ea
Jeremiah Quaynor <73517688+Jeremiah-Quaynor@users.noreply.github.com>
2021-11-25 21:33:31 +0000

    Add files via upload
    creating the home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './components/home/home';
+import { Route, Routes } from 'react-router';
+import Recipes from './components/recipies/recipes';
+// import FoodAndRecipe from './components/recipies/foodAndRecipe';
+
 
 
 function App() {
   return (
     <>
-    <Home/>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/home" element={<Home/>} />
+      <Route path='/recipes' element={<Recipes />} />
+    </Routes>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
+import { BrowserRouter } from 'react-router-dom';
+
 
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App/>
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
