b838a22f3a1762d4785fc0776ecce6c3e8d8c5f6
Jeremiah Quaynor <73517688+Jeremiah-Quaynor@users.noreply.github.com>
2021-12-20 06:44:12 +0000

    bug fixes

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,25 +5,25 @@ import classes from './AvailableMeals.module.css';
 const DUMMY_MEALS = [
   {
     id: 'm1',
-    name: 'Banku with Tilapia',
-    description: 'Finest fish and banku',
+    name: 'Sushi',
+    description: 'Finest fish and veggies',
     price: 22.99,
   },
   {
     id: 'm2',
-    name: 'Pizza',
-    description: 'An Italian specialty!',
+    name: 'Schnitzel',
+    description: 'A german specialty!',
     price: 16.5,
   },
   {
     id: 'm3',
-    name: 'Fried Rice',
-    description: 'Ghanaian, rice and veggies',
+    name: 'Barbecue Burger',
+    description: 'American, raw, meaty',
     price: 12.99,
   },
   {
     id: 'm4',
-    name: 'Ampesi with Abom',
+    name: 'Green Bowl',
     description: 'Healthy...and green...',
     price: 18.99,
   },
